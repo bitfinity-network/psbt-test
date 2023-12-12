afd1c4e5b7bf06055b2bab651f507bb20a58fec5
veeso <christian.visintin@veeso.dev>
2023-12-12 09:24:11 +0100

    Never gonna let you down

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,19 @@ mod utils;
 
 use std::str::FromStr;
 
+use bip39::Mnemonic;
 use bitcoin::absolute::LockTime;
+use bitcoin::bip32::ChildNumber;
+use bitcoin::bip32::DerivationPath;
+use bitcoin::bip32::Xpriv;
+use bitcoin::bip32::Xpub;
 use bitcoin::opcodes::all::{OP_CHECKSIG, OP_ENDIF, OP_IF};
 use bitcoin::opcodes::{OP_0, OP_FALSE};
 use bitcoin::script::Builder as ScriptBuilder;
 use bitcoin::transaction::Version;
 use bitcoin::{
     secp256k1::{All, Secp256k1},
-    Address, Amount, PrivateKey, PublicKey, Txid,
+    Address, Amount, PublicKey, Txid,
 };
 use bitcoin::{Network, OutPoint, ScriptBuf, Sequence, Transaction, TxIn, TxOut, Witness};
 use ord_rs::Inscription as _;
@@ -25,11 +30,14 @@ use ord_rs::{transaction::TxInput, OrdError};
 use crate::utils::bytes_to_push_bytes;
 
 /// tb1qzc8dhpkg5e4t6xyn4zmexxljc4nkje59dg3ark
-const SENDER_ADDRESS_WIF: &str = "cVkWbHmoCx6jS8AyPNQqvFr8V9r2qzDHJLaxGDQgDJfxT73w6fuU";
+const SENDER_ADDRESS_MNEMONIC: &str =
+    "educate loyal echo sphere near family potato proud fresh still hub address";
 /// tb1qax89amll2uas5k92tmuc8rdccmqddqw94vrr86
-const RECIPIENT_ADDRESS_WIF: &str = "cVpCkgJsvYyCuARLa35jkxmXJoJW2gCbuV5eMNeEESpWz1pUhyub";
+const RECIPIENT_ADDRESS_MNEMONIC: &str =
+    "yard arctic apart velvet virus flight lemon cable ozone pole course awake";
 /// tb1qcwflhw3252daxhj6d40wxpuard5c05lzqptdx7
-const MARKETPLACE_ADDRESS_WIF: &str = "cR1HwkKsYpMxbAzJNzrW8hpBBRV11G5zb5RQfKzD4bCHkSwrZw7x";
+const MARKETPLACE_ADDRESS_MNEMONIC: &str =
+    "position goat expect abandon mesh response champion list praise broccoli orange pole";
 
 const COMMIT_FEE: u64 = 2_500;
 const REVEAL_FEE: u64 = 4_700;
@@ -39,19 +47,34 @@ const POSTAGE: u64 = 333;
 pub struct Account {
     pub address: Address,
     pub public_key: PublicKey,
-    pub private_key: PrivateKey,
+    pub private_key: Xpriv,
+    pub input_xpub: Xpub,
+    pub path: DerivationPath,
 }
 
 impl Account {
-    pub fn from_wif(secp: &Secp256k1<All>, wif: &str) -> anyhow::Result<Self> {
-        let private_key = PrivateKey::from_wif(wif)?;
-        let public_key = private_key.public_key(secp);
-        let address = Address::p2wpkh(&public_key, bitcoin::Network::Testnet)?;
+    pub fn from_mnemonic(secp: &Secp256k1<All>, mnemonic: &str) -> anyhow::Result<Self> {
+        let mnemonic = Mnemonic::from_str(mnemonic)?;
+        let seed = mnemonic.to_seed("");
+        let root = Xpriv::new_master(Network::Testnet, &seed)?;
+
+        // derive child xpub
+        let path = DerivationPath::from_str("m/84h/0h/0h")?;
+        let child = root.derive_priv(&secp, &path)?;
+        let xpub = Xpub::from_priv(&secp, &child);
+
+        let zero = ChildNumber::from_normal_idx(0)?;
+        let public_key = xpub.derive_pub(&secp, &[zero, zero])?.public_key;
+
+        let public_key = PublicKey::new(public_key);
+        let address = Address::p2wpkh(&public_key, Network::Testnet)?;
 
         Ok(Self {
             address,
             public_key,
-            private_key,
+            private_key: root,
+            input_xpub: xpub,
+            path,
         })
     }
 }
@@ -62,9 +85,13 @@ async fn main() -> anyhow::Result<()> {
 
     let secp = Secp256k1::new();
     // setup accounts
-    let sender = Account::from_wif(&secp, SENDER_ADDRESS_WIF)?;
-    let recipient = Account::from_wif(&secp, RECIPIENT_ADDRESS_WIF)?;
-    let marketplace = Account::from_wif(&secp, MARKETPLACE_ADDRESS_WIF)?;
+    let sender = Account::from_mnemonic(&secp, SENDER_ADDRESS_MNEMONIC)?;
+    let recipient = Account::from_mnemonic(&secp, RECIPIENT_ADDRESS_MNEMONIC)?;
+    let marketplace = Account::from_mnemonic(&secp, MARKETPLACE_ADDRESS_MNEMONIC)?;
+
+    debug!("sender: {}", sender.address);
+    debug!("recipient: {}", recipient.address);
+    debug!("marketplace: {}", marketplace.address);
 
     // input to use
     let tx_input = TxInput {
@@ -149,6 +176,7 @@ async fn main() -> anyhow::Result<()> {
     let partially_signed_tx = psbt::sign_partially(
         &secp,
         unsigned_tx,
+        &marketplace,
         &[sender.clone(), recipient],
         TxOut {
             value: tx_input.amount,
@@ -156,7 +184,11 @@ async fn main() -> anyhow::Result<()> {
         },
         &redeem_script,
     )?;
-    let mut signer = signer::Signer::new(&marketplace.private_key, &secp, partially_signed_tx);
+    debug!("partially_signed_tx: {partially_signed_tx:?}");
+
+    // sign
+    let sign_key = sender.private_key.to_priv();
+    let mut signer = signer::Signer::new(&sign_key, &secp, partially_signed_tx);
     let signed_tx = signer.sign_commit_transaction(&[tx_input], &sender.address.script_pubkey())?;
     debug!("signed_tx: {signed_tx:?}");
 
--- src/psbt.rs
+++ src/psbt.rs
@@ -3,7 +3,7 @@ use std::{collections::BTreeMap, str::FromStr};
 use bitcoin::{
     psbt::{Input, PsbtSighashType},
     secp256k1::{All, Secp256k1},
-    Psbt, ScriptBuf, Transaction, TxOut,
+    Psbt, ScriptBuf, Transaction, TxOut, Witness,
 };
 
 use crate::Account;
@@ -11,13 +11,13 @@ use crate::Account;
 pub fn sign_partially(
     secp: &Secp256k1<All>,
     unsigned_tx: Transaction,
+    updater_account: &Account,
     accounts: &[Account],
     previous_output: TxOut,
-    redeem_script: &ScriptBuf,
+    witness_script: &ScriptBuf,
 ) -> anyhow::Result<Transaction> {
     // Creator (https://github.com/rust-bitcoin/rust-bitcoin/blob/master/bitcoin/examples/ecdsa-psbt.rs)
     let mut psbt = Psbt::from_unsigned_tx(unsigned_tx)?;
-    debug!("unsigned psbt: {psbt:?}");
 
     // updater
     let mut input = Input {
@@ -25,24 +25,54 @@ pub fn sign_partially(
         ..Default::default()
     };
 
-    input.redeem_script = Some(redeem_script.clone());
+    let pk = updater_account.input_xpub.to_pub();
+    let wpkh = pk.wpubkey_hash().expect("a compressed pubkey");
+
+    let redeem_script = ScriptBuf::new_p2wpkh(&wpkh);
+    input.redeem_script = Some(redeem_script);
+
+    let fingerprint = updater_account.private_key.fingerprint(secp);
+    let mut map = BTreeMap::new();
+    map.insert(pk.inner, (fingerprint, updater_account.path.clone()));
+    input.bip32_derivation = map;
 
     let ty = PsbtSighashType::from_str("SIGHASH_ALL")?;
     input.sighash_type = Some(ty);
 
     psbt.inputs = vec![input];
+    debug!("unsigned psbt: {psbt:#?}");
 
     // sign
-    let mut sigs = BTreeMap::new();
-    for account in accounts {
-        sigs.insert(account.public_key, account.private_key);
+    match psbt.sign(&updater_account.private_key, secp) {
+        Ok(keys) if keys.len() == 1 => {}
+        Ok(_) => anyhow::bail!("unexpected number of keys"),
+        Err(_) => anyhow::bail!("signing failed"),
     }
-    match psbt.sign(&sigs, secp) {
-        Ok(keys) => assert_eq!(keys.len(), 1),
-        Err((_, e)) => {
-            anyhow::bail!("signing failed: {:?}", e);
+    for account in accounts {
+        match psbt.sign(&account.private_key, secp) {
+            Ok(keys) if keys.len() == 1 => {}
+            Ok(_) => anyhow::bail!("unexpected number of keys"),
+            Err(_) => anyhow::bail!("signing failed"),
         }
-    };
+    }
+
+    debug!("signed psbt: {psbt:#?}");
+
+    // Push witness
+    let sigs: Vec<_> = psbt.inputs[0].partial_sigs.values().collect();
+    let mut script_witness = Witness::new();
+    script_witness.push(&sigs[0].to_vec());
+    script_witness.push(witness_script);
+
+    // Clear all the data fields as per the spec.
+    debug!("finalized psbt: {psbt:#?}");
+    psbt.inputs[0].partial_sigs = BTreeMap::new();
+    psbt.inputs[0].sighash_type = None;
+    psbt.inputs[0].redeem_script = None;
+    psbt.inputs[0].witness_script = None;
+    psbt.inputs[0].bip32_derivation = BTreeMap::new();
+
+    psbt.inputs[0].final_script_witness = Some(script_witness);
 
     Ok(psbt.extract_tx_fee_rate_limit()?)
 }
